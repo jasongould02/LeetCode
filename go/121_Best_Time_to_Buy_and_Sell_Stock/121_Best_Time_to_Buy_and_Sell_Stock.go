d1d80ecce8cea1826344e99e08ee4e4f58fa2628
Jason Gould <15948550+jasongould02@users.noreply.github.com>
2022-11-21 08:30:33 -0500

    added go solutions for problems 121 and 278

---
+++ go/121_Best_Time_to_Buy_and_Sell_Stock/121_Best_Time_to_Buy_and_Sell_Stock.go
@@ -0,0 +1,16 @@
+func maxProfit(prices []int) int {
+    lsf := math.MaxInt
+    op := 0
+    pist := 0
+
+    for i := 0; i < len(prices); i++ {
+        if prices[i] < lsf {
+            lsf = prices[i]
+        }
+        pist = prices[i] - lsf
+        if op < pist {
+            op = pist
+        }
+    }
+    return op
+}
