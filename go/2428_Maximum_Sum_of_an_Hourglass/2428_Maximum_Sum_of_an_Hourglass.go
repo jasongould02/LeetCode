9f81c9f0e1e25d258a093d79467f4e903f67a8fa
Jason Gould <15948550+jasongould02@users.noreply.github.com>
2023-01-05 16:51:41 -0500

    added go solutions for problems 21 and 2428

---
+++ go/2428_Maximum_Sum_of_an_Hourglass/2428_Maximum_Sum_of_an_Hourglass.go
@@ -0,0 +1,12 @@
+func maxSum(grid [][]int) int {
+    max := 0
+    for i := 0; i < len(grid)-2; i++ {
+        for j := 0; j < len(grid[0])-2; j++ {
+            sum := grid[i][j] + grid[i][j+1] + grid[i][j+2] + grid[i+1][j+1] + grid[i+2][j] + grid[i+2][j+1] + grid[i+2][j+2]
+            if sum > max {
+                max = sum
+            }
+        }
+    }
+    return max
+}
