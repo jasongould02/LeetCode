4ae046c98431489588b0dcff595239a5d5bcc18a
Jason Gould <15948550+jasongould02@users.noreply.github.com>
2023-01-12 15:51:01 -0500

    added go solution for problem 283

---
+++ go/283_Move_Zeroes/283_Move_Zeroes.go
@@ -0,0 +1,13 @@
+func moveZeroes(nums []int)  {
+    for i := 0; i < len(nums); i++ {
+        if nums[i] == 0 {
+            for j := i+1; j < len(nums); j++ {
+                if nums[j] != 0 {
+                    nums[i] = nums[j]
+                    nums[j] = 0
+                    break
+                }
+            }
+        }
+    }
+}
